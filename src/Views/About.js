48b90a548aa7ba8119f068384703f80ca24ad522
Fang Xu <xufang5555@hotmail.com>
2022-04-04 11:08:10 -0400

    added google analytics

--- src/Views/About.js
+++ src/Views/About.js
@@ -6,17 +6,18 @@ import {AboutStrings} from 'Utils/Strings';
 const About = () => {
     return (
         <div className="about-container view-container">
-            {/*{AboutStrings.map((item, i) =>*/}
-            {/*    <ExperienceCard*/}
-            {/*        orientation={i % 2 === 0 ? 'left' : 'right'}*/}
-            {/*        title={item.title}*/}
-            {/*        color={item.color}*/}
-            {/*        subtitle={item.subtitle}*/}
-            {/*        imgSrc={item.imgSrc}*/}
-            {/*    >*/}
-            {/*        {item.detail}*/}
-            {/*    </ExperienceCard>*/}
-            {/*)}*/}
+            {AboutStrings.map((item, i) =>
+                <ExperienceCard
+                    orientation={i % 2 === 0 ? 'left' : 'right'}
+                    title={item.title}
+                    color={item.color}
+                    subtitle={item.subtitle}
+                    imgSrc={item.imgSrc}
+                    key={i}
+                >
+                    {item.detail}
+                </ExperienceCard>
+            )}
             <div style={{textAlign: 'center', marginTop: '30%'}}>Coming soon...</div>
         </div>
     );
