fc63b43daae8ede8151d827594da406b750b1f05
Fang Xu <xufang5555@hotmail.com>
2022-04-08 17:09:35 -0400

    finished contact page

--- src/Views/Contact.js
+++ src/Views/Contact.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import './Contact.scss';
-import {isProdEnv} from 'Utils/constants';
 import ContactForm from 'Components/Forms/ContactForm';
 
 const Contact = () => {
     return (
         <div className="contact-container view-container">
-            {isProdEnv==="production" ?
-                <div style={{textAlign: 'center', marginTop: '30%'}}>Coming soon...</div>
-                :
-                <div className="contact">
-                    <div className="contact-avatar flex-center">
-                        <img src="assets/fx.jpeg" alt="avatar"/>
-                    </div>
-                    <div>
-                        <ContactForm />
-                    </div>
+            <div className="contact">
+                <div className="contact-avatar flex-center">
+                    <img src="assets/fx.jpeg" alt="avatar"/>
                 </div>
-            }
+                <div>
+                    <ContactForm />
+                </div>
+            </div>
         </div>
     );
 };
