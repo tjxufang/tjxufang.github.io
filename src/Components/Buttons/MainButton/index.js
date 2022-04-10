4df437dbfbddfc2c79dccf4735e1d58b857b5265
Fang Xu <xufang5555@hotmail.com>
2022-04-10 01:21:30 -0400

    homepage responsive done; button bg img added

--- src/Components/Buttons/MainButton/index.js
+++ src/Components/Buttons/MainButton/index.js
@@ -4,6 +4,9 @@ import './index.scss'
 const MainButton = ({children, disabled, disabledText}) => {
     return (
         <button className="main-button" disabled={disabled}>
+            <div className="bg-img">
+                <img src="assets/btn-bg.png" alt="button-background"/>
+            </div>
             {disabled ? disabledText : children}
         </button>
     );
